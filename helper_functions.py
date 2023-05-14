522f12b9d2a9b1412e4d6882c967285e5b290ce9
Omar Mohamed <64428610+skittoo@users.noreply.github.com>
2023-05-14 02:30:01 +0200

    latest

--- helper_functions.py
+++ helper_functions.py
@@ -7,7 +7,7 @@ from typing import List, Dict
 def validate_rss_link(link):
     """Validate it's an Upwork rss url."""
     parsed_link = urlparse(link)
-    if ((parsed_link.scheme == 'https') and (parsed_link.netloc == 'www.upwork.com') and ('rss' in parsed_link.path) and (link.count("https") == 1) ):
+    if ( (parsed_link.scheme == 'https') and (parsed_link.netloc == 'www.upwork.com') and ('rss' in parsed_link.path) and (link.count("https") == 1) ):
       return True
     else:
       return False
@@ -17,29 +17,46 @@ def parse_job_listing(text: str):
     info_dict = {}
     # Remove HTML tags
     text_no_html = no_html_text(text)
-    
-    # Extract Budget or Hourly Range
-    if "Budget" in text_no_html:
-        info_dict['description'] = text_no_html[:text_no_html.find('Budget')] 
-        info_dict['price'] =  re.search("Budget: (.*)", text_no_html).group(1)
-        info_dict['hourly_range'] = False
 
-    elif "Hourly Range" in text_no_html:
-        info_dict['description'] = text_no_html[:text_no_html.find('Hourly Range')]
-        info_dict['price'] = re.search("Hourly Range: (.*)", text_no_html).group(1)
-        info_dict['hourly_range'] = True
+    try:    
+      # Extract Budget or Hourly Range
+      if "Budget" in text_no_html:
+          info_dict['description'] = text_no_html[:text_no_html.find('Budget')] 
+          info_dict['price'] =  re.search("Budget: (.*)", text_no_html).group(1)
+          info_dict['hourly_range'] = False
 
+      elif "Hourly Range" in text_no_html:
+          info_dict['description'] = text_no_html[:text_no_html.find('Hourly Range')]
+          info_dict['price'] = re.search("Hourly Range: (.*)", text_no_html).group(1)
+          info_dict['hourly_range'] = True
+    except:
+          info_dict['description'] = "Null"
+          info_dict['price'] =  "Null"
+          info_dict['hourly_range'] = "Null"
+      
     # Extract Posted On
-    info_dict['posted_on'] = re.search("Posted On: (.*) UTC", text_no_html).group(1)
+    try:
+      info_dict['posted_on'] = re.search("Posted On: (.*) UTC", text_no_html).group(1)
+    except:
+      info_dict['posted_on'] = "Null"
 
     # Extract Category
-    info_dict['category'] = re.search("Category: (.*)", text_no_html).group(1)
+    try:
+      info_dict['category'] = re.search("Category: (.*)", text_no_html).group(1)
+    except:
+      info_dict['category'] = "Null"
 
-    # Extract Skills
-    info_dict['skills'] = re.search("Skills:(.*)", text_no_html).group(1)
+    # Extract Skills    
+    try:
+      info_dict['skills'] = re.search("Skills:(.*)", text_no_html).group(1)
+    except:
+      info_dict['skills'] = "Null"
 
     # Extract Country
-    info_dict['country'] = re.search("Country: (.*)", text_no_html).group(1)
+    try:
+      info_dict['country'] = re.search("Country: (.*)", text_no_html).group(1)
+    except:
+      info_dict['country'] = "Null"
 
     return info_dict
 
@@ -57,6 +74,7 @@ def no_html_text(text: str):
     return text_no_html
 
 
+
 def is_job_post_exist(job_link: str, all_jobs_info_old: List[Dict]):
     for job_dict in all_jobs_info_old:
         if job_link == job_dict['link']:
@@ -69,16 +87,44 @@ def parse_rss(rss_url: str, all_jobs_info_old: List[Dict]):
     feed = feedparser.parse(rss_url)
     # Saving all jobs info.
     all_jobs_info = []
-    for entry in feed.entries:
-        if is_job_post_exist(entry.link, all_jobs_info_old):
-            continue
+    printable_rss_feed = []
+    for entry in feed.entries:        
+      
         info_dict = {}
         info_dict['link'] = entry.link
         info_dict['title'] = entry.title
-        # Adding more information to the job post dictionary.
-        info_dict.update(parse_job_listing(rss_url))
+        info_dict.update(parse_job_listing(entry.description))
         all_jobs_info.append(info_dict)
 
-    return all_jobs_info
+        if is_job_post_exist(entry.link, all_jobs_info_old):
+            continue
+
+        printable_rss_feed.append(info_dict)
+
+    return printable_rss_feed, all_jobs_info
+
+def update_rss_links(rss_urls):
+    printable_rss_feed = {}
+    new_rss_urls = {}
+    for rss_url in rss_urls:
+        printable_rss_feed[rss_url], new_rss_urls[rss_url] = parse_rss(rss_url, rss_urls[rss_url])
+
+    return printable_rss_feed, new_rss_urls
 
 
+def convert_dict_to_message(info_dict):
+    try:
+        price_type = "🕐 Hourly Rate" if info_dict.get('hourly_range', False) else "💵 Fixed Price"
+        message = (f"🔖 Title: {info_dict['title']}\n"
+                   f"🔗 Link: {info_dict['link']}\n"
+                   f"📝 Description: {info_dict['description']}\n"
+                   f"{price_type}: {info_dict['price']}\n"
+                   f"⏰ Posted On: {info_dict['posted_on']}\n"
+                   f"🏷️ Category: {info_dict['category']}\n"
+                   f"🛠️ Skills: {info_dict['skills']}\n"
+                   f"🌍 Country: {info_dict['country']}\n")
+        return message
+    except:
+        message = (f"🔖 Title: {info_dict['title']}\n"
+                   f"🔗 Link: {info_dict['link']}\n")
+        return message
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 import threading
 from dotenv import load_dotenv
 import os
-from helper_functions import validate_rss_link
+from helper_functions import validate_rss_link, update_rss_links, convert_dict_to_message
 
 
 load_dotenv()
@@ -12,14 +12,24 @@ load_dotenv()
 bot = telebot.TeleBot(f"{os.environ['BOT_TOKEN']}")
 
 chat_ids = [] # A list of chat ids to send messages for.
-rss_links = {} # Dictionary for saving all rss links and it's latest 10 job posts.
+rss_urls = {} # Dictionary for saving all rss links and it's latest 10 job posts.
 
 def job():
     """Scheduled job for getting all RSS links updates."""
     global chat_ids
-    if chat_ids:
-        for chat_id in chat_ids:
-            bot.send_message(chat_id, "Message")
+    global rss_urls
+    if rss_urls:
+        printable_rss_urls, rss_urls = update_rss_links(rss_urls)
+    else:
+        return   
+
+    for rss_url in printable_rss_urls:
+        for job_post in printable_rss_urls[rss_url]:
+            if chat_ids:
+                for chat_id in chat_ids:
+                    message = convert_dict_to_message(job_post)
+                    bot.send_message(chat_id, message)
+            
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -35,17 +45,19 @@ def send_help(message):
 
 @bot.message_handler(func=lambda message: True)
 def check_rss(message):
-    global rss_links
-    if validate_rss_link(message.text):
+    global rss_urls
+    if not validate_rss_link(message.text):
         bot.reply_to(message, "Invalid RSS link, please try again. see: https://github.com/skittoo/upwork-telegram-bot")
     else:
-        if message.text in list(rss_links.keys()):
+        if message.text in list(rss_urls.keys()):
             bot.reply_to(message, "RSS already saved.")
         else:
-            rss_links[message.text] = [] # Make an empty list of the job posts.
+            rss_urls[message.text] = [] # Make an empty list of the job posts.
+            bot.reply_to(message, "Got your RSS url.")
+
                         
 # Scheduling a task for checking rss links.                       
-schedule.every(os.environ['TIME_INTERVAL']).seconds.do(job)
+schedule.every(int(os.environ['TIME_INTERVAL'])).seconds.do(job)
 
 def run_schedule():
     while True:
